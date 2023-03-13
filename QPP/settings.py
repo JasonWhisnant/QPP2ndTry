e7f512cf1e2c6ecbf71448a078126b1003828ec2
JasonWhisnant <124310379+JasonWhisnant@users.noreply.github.com>
2023-03-13 11:53:04 -0500

    Update settings.py

--- QPP/settings.py
+++ QPP/settings.py
@@ -32,6 +32,7 @@ DEBUG = True
 INSTALLED_APPS = [
     'dal',
     'dal_select2',
+    'django_extensions'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
