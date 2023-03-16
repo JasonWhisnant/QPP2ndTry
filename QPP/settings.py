fdf0c5695564f60f2c54417a74f8d1e02217ae47
JasonWhisnant <124310379+JasonWhisnant@users.noreply.github.com>
2023-03-16 10:52:26 -0500

    Update static folder location in settings

--- QPP/settings.py
+++ QPP/settings.py
@@ -148,7 +148,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'EmpReview/static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
