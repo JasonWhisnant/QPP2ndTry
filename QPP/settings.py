0b5046238c9719494e6b76c2d0508310dadadebc
JasonWhisnant <124310379+JasonWhisnant@users.noreply.github.com>
2023-03-20 13:21:37 -0500

    update to models

--- QPP/settings.py
+++ QPP/settings.py
@@ -102,15 +102,11 @@ OKTA_AUTH = {
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
-conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': conn_str_params['dbname'],
-        'HOST': conn_str_params['host'],
-        'USER': conn_str_params['user'],
-        'PASSWORD': conn_str_params['password'],
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
     }
 }
 
