ff19306df20ccd3ab717dbad6de3ad19d102c968
JasonWhisnant <124310379+JasonWhisnant@users.noreply.github.com>
2023-04-06 14:55:38 -0500

    Removed secret key and moved to server settings

--- QPP/settings.py
+++ QPP/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u&)3i60w4nabvbmgfp)ecm+w$3_8n9p3c=#*855i!xbz^s=2nk'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
